b0e41427c354339a4db2382c722d56125021dc84
cdcm <carloss_jt@hotmail.com>
2024-12-25 15:21:38 -0500

    chore: add supabase middleware created middleware to handle auth tokens extended request parameter to manage supabase client

---
+++ src/middlewares/supabaseTokenMiddleware.ts
@@ -0,0 +1,25 @@
+import { Response, NextFunction, Request } from "express"
+import { getSupabaseClient } from "@/config/supabaseClient"
+
+export const attachSupabaseClient = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1]
+  const refreshToken = req.headers["x-refresh-token"] as string
+  if (!token || !refreshToken) {
+    return res.status(401).json({ error: "Unauthorized" })
+  }
+  const supabase = getSupabaseClient()
+  const { error } = await supabase.auth.setSession({
+    access_token: token,
+    refresh_token: refreshToken
+  })
+  if (error) {
+    console.log(error)
+    return res.status(401).json({ error: "Unauthorized" })
+  }
+  req.supabase = supabase
+  next()
+}

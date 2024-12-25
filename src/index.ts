e7b1e2afbf1f1ed28c741b0ad7df9dfc145dcd43
cdcm <carloss_jt@hotmail.com>
2024-12-24 22:31:42 -0500

    feat: initialize project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express()
+
+
+app.use(morgan("dev"))
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})

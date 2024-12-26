d535dd9d0f601b397c3a0859d29e752a37284608
cdcm <carloss_jt@hotmail.com>
2024-12-26 12:57:18 -0500

    chore: refactor code

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -10,10 +10,13 @@ import { Request, Response } from "express"
 export const getTasksController = async (req: Request, res: Response) => {
   try {
     const supabase = req.supabase
-    const {error, data} = await supabase.auth.getUser()
+    const { error, data } = await supabase.auth.getUser()
     if (error) return res.status(401).json({ error: "Unauthorized" })
 
-    const {error: taskError, data: tasksData} = await getTasksService(supabase, data.user?.id)
+    const { error: taskError, data: tasksData } = await getTasksService(
+      supabase,
+      data.user?.id
+    )
     if (taskError) {
       return res.status(400).json({ error: taskError.message })
     }
@@ -27,14 +30,14 @@ export const getTasksController = async (req: Request, res: Response) => {
 export const postTaskController = async (req: Request, res: Response) => {
   try {
     const supabase = req.supabase
-    const {error: userError, data: userData} = await supabase.auth.getUser()
+    const { error: userError, data: userData } = await supabase.auth.getUser()
     if (userError) return res.status(401).json({ error: "Unauthorized" })
 
     const { title, description, state } = req.body
     if (!title || !description || state === undefined)
       return res.status(400).json({ error: "Missing required fields" })
 
-    const {error, data} = await postTaskService(supabase, {
+    const { error, data } = await postTaskService(supabase, {
       id_user: userData.user!.id,
       title,
       description,
@@ -54,7 +57,7 @@ export const postTaskController = async (req: Request, res: Response) => {
 export const PutTaskController = async (req: Request, res: Response) => {
   try {
     const supabase = req.supabase
-    const {error, data: userData} = await supabase.auth.getUser()
+    const { error, data: userData } = await supabase.auth.getUser()
     if (error) return res.status(401).json({ error: "Unauthorized" })
 
     const taskId = req.params.id
@@ -65,19 +68,23 @@ export const PutTaskController = async (req: Request, res: Response) => {
     if (!title || !description || state === undefined)
       return res.status(400).json({ error: "Missing required fields" })
 
-    const exist = await verifyExistenceService(supabase, taskId, userData.user!.id)
+    const exist = await verifyExistenceService(
+      supabase,
+      taskId,
+      userData.user!.id
+    )
     if (!exist) {
       return res.status(400).json({ error: "Task not found" })
-    }   
+    }
 
-    const {error: updateError, data: taskData} = await putTaskService(
+    const { error: updateError, data: taskData } = await putTaskService(
       supabase,
       {
         id_user: userData.user!.id,
         title,
         description,
         state,
-        id: taskId
+        id: taskId,
       }
     )
     if (updateError) {
@@ -92,27 +99,37 @@ export const PutTaskController = async (req: Request, res: Response) => {
 }
 
 export const deleteTaskController = async (req: Request, res: Response) => {
-  try{
+  try {
     const supabase = req.supabase
-    const {error, data: userData} = await supabase.auth.getUser()
+    const { error, data: userData } = await supabase.auth.getUser()
     if (error) return res.status(401).json({ error: "Unauthorized" })
 
     const taskId = req.params.id
     if (taskId === undefined)
       return res.status(400).json({ error: "Missing required fields" })
 
-    const exist = await verifyExistenceService(supabase, taskId, userData.user!.id)
+    const exist = await verifyExistenceService(
+      supabase,
+      taskId,
+      userData.user!.id
+    )
     if (!exist) {
       return res.status(400).json({ error: "Task not found" })
     }
-    
-    const {error: deleteError} = await deleteTaskService(supabase, taskId, userData.user!.id)
+
+    const { error: deleteError } = await deleteTaskService(
+      supabase,
+      taskId,
+      userData.user!.id
+    )
     if (deleteError) {
       return res.status(400).json({ error: deleteError.message })
     }
 
-    return res.status(200).json({ message: "Task deleted successfully" })
-  } catch(error) {
+    return res
+      .status(200)
+      .json({ message: "Task deleted successfully", id: taskId })
+  } catch (error) {
     return res.status(500).json({ error: "Internal server error" })
   }
 }

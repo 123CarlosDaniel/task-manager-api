99ab8ebd141ee5f16494683bf3e18b6e554505c1
cdcm <carloss_jt@hotmail.com>
2024-12-27 10:43:56 -0500

    chore: update corsConfig

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -2,7 +2,7 @@ import { APP_URL } from "@/lib/vars"
 import cors from "cors"
 
 export const corsOptions = {
-  origin: ["*", APP_URL],
+  origin: ["http://localhost:5173", APP_URL],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-refresh-token"],
   credentials: true,

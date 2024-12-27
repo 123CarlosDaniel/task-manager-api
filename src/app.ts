f648c0857558ad8a990fca0c47d3293c5b171f39
cdcm <carloss_jt@hotmail.com>
2024-12-27 01:17:12 -0500

    feat: add swagger for documentation

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,40 @@ import authRoutes from "@/routes/auth.routes"
 import taskRoutes from "@/routes/task.routes"
 import cors from "cors"
 import { corsOptions } from "./config/corsConfig"
+import swaggerUi from "swagger-ui-express"
+import { middleware } from "express-openapi-validator"
+import path from "path"
 
 const app = express()
+app.use(express.json())
+
+// configurar swagger UI
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+)
+
+// Servir el archivo openapi
+app.use(
+  "/openapi.json",
+  express.static(path.join(__dirname, "../openapi.yaml"))
+)
+
+// Validar las rutas con OpenAPI
+app.use(
+  middleware({
+    apiSpec: path.join(__dirname, "../openapi.yaml"),
+    validateRequests: true, // Validar las solicitudes entrantes
+  })
+)
 
 app.use(cors(corsOptions))
 app.use(morgan("dev"))
-app.use(express.json())
-
 
 app.use("/api/auth", authRoutes)
 app.use("/api/tasks", taskRoutes)
